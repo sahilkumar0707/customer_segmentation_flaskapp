5b05d8ae0b2d98c762061ef069912d6cac827c9b
sahilkared <work.sahilkared22@gmail.com>
2023-04-14 18:42:05 -0400

    intial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+app1 = Flask(__name__)
+segment_model = pickle.load(open('knn_model.pkl','rb'))
+@app1.route('/')
+def index():
+    return render_template('index.html')
+
+@app1.route('/predict', methods =['POST'])
+def predict_segment():
+    Gender = int(request.form.get('Gender'))
+    Ever_Married = int(request.form.get('Ever_Married'))
+    Age = int(request.form.get('Age'))
+    Profession = int(request.form.get('Profession'))
+    Work_Experience = int(request.form.get('Work_Experience'))
+    Spending_Score = int(request.form.get('Spending_Score'))
+    Family_Size = int(request.form.get('Family_Size'))
+    # prediction going on
+    result = segment_model.predict(np.array([Gender, Ever_Married, Age, Profession, Work_Experience, Spending_Score, Family_Size]).reshape(1, 7))
+    return str(result)
+
+if __name__ == '__main__':
+    app1.run(debug=True)
