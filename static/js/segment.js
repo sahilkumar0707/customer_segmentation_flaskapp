3eb6b8e5a4283ff7159382157da70fdd4a7b5360
sahilkared <work.sahilkared22@gmail.com>
2023-05-05 18:24:55 -0400

    addtion of form validation

--- static/js/segment.js
+++ static/js/segment.js
@@ -3,7 +3,7 @@ function validation()
 	var age = document.getElementById('age').value;
 	var family_size = document.getElementById('family_size').value;
 	if(age == "" || family_size == ""){
-		alert('please feel age or family size');
+		alert('please fill age or family size');
 		return false;
 
 
